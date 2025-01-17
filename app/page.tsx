4cce89bf8e5d1ee0ab5856549bf0081a5ee2aacf
Reyhan Vivaldi Adrian <reyhanvivaldi@reyv-2.local>
2025-01-17 13:37:02 +0700

    REFACTOR - Delete meta tag on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import ActivityList from "@/components/home/ActivityList";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
-      <meta name="agd-partner-manual-verification" />
       {/* <div className="flex flex-col items-center py-3 md:py-8 px-5"> */}
       <div className="w-full max-w-6xl mx-auto">
         <Banner />
