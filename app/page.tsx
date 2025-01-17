5f4267c6ad671a9e5fed51d89f44c5aaaf499f96
Reyhan Vivaldi Adrian <reyhanvivaldi@reyv-2.local>
2025-01-17 11:13:47 +0700

    CORE - Create section Cari Hotel, Aktivitas. Delete ItineraryHeader. Refine UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import Banner from "@/components/home/Banner";
-import ItineraryListHeader from "@/components/home/ItineraryListHeader";
-import ItineraryList from "@/components/home/ItineraryList";
+import ItineraryList from "@/components/home/ItineraryList"; 
+import HotelList from "@/components/home/HotelList";
+import ActivityList from "@/components/home/ActivityList";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
-      <div className="flex flex-col items-center py-3 md:py-8 px-5">
+      {/* <div className="flex flex-col items-center py-3 md:py-8 px-5"> */}
+      <div className="w-full max-w-6xl mx-auto">
         <Banner />
-        {/* <ItineraryListHeader /> */}
-        <ItineraryList/>
+
+        <div className="mt-10">
+          <ItineraryList />
+        </div>
+
+        {/* Section 2: CariHotel */}
+        <div className="mt-10">
+          <HotelList />
+        </div>
+
+        {/* Section 3: CariAktivitas */}
+        <div className="mt-10">
+          <ActivityList />
+        </div>
+        
       </div>
     </main>
   );
